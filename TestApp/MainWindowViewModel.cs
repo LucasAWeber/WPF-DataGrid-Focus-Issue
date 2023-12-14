746d829741084dfc1e733ed6834dca7ecf9cee34
Lucas Weber <87451369+LucasAWeber@users.noreply.github.com>
2023-12-14 13:53:47 -0500

    Add files via upload

---
+++ TestApp/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class TestObj
+    {
+        public string Name { get; set; } = "";
+        public string Text { get; set; } = "";
+    }
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<TestObj> test;
+
+        public MainWindowViewModel()
+        {
+            Test = new ObservableCollection<TestObj>();
+            Populate();
+        }
+
+        [RelayCommand]
+        private void Populate()
+        {
+            Test.Clear();
+            Test.Add(new TestObj()
+            {
+                Name = "Test1",
+                Text = "Test1",
+            });
+            Test.Add(new TestObj()
+            {
+                Name = "Test2",
+                Text = "Test2",
+            });
+            Test.Add(new TestObj()
+            {
+                Name = "Test3",
+                Text = "Test3",
+            });
+            Test.Add(new TestObj()
+            {
+                Name = "Test4",
+                Text = "Test4",
+            });
+        }
+    }
+}
